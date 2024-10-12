37faed9f2c31a09f640d1a0fe55f3d90f6670cb8
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2024-10-13 01:11:06 +0700

    Menambahkan fungsi uppercaseString dan contoh penggunaan lambda untuk mengubah string menjadi huruf besar.

---
+++ app/src/main/java/com/example/questbasickotlin_0115/LambdaFunction.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan1
+
+fun uppercaseString(String: String): String{
+    return String.uppercase()
+}
+
+fun main(){
+    uppercaseString("hello")
+    println({string: String -> string.uppercase()}("hello"))
+    //hello
+}
