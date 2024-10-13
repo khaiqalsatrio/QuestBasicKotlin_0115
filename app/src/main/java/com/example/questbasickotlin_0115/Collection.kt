a7040f04d3642e712da6293592a0dc8f59520dd9
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2024-10-13 09:08:48 +0700

    Untuk commit yang mencakup penambahan, penghapusan, pengubahan, dan pembacaan data pada list, kamu bisa menggunakan kata commit berikut:
    Manage mutable list operations: add, remove, update, and read-only view

--- app/src/main/java/com/example/questbasickotlin_0115/Collection.kt
+++ app/src/main/java/com/example/questbasickotlin_0115/Collection.kt
@@ -9,4 +9,20 @@ fun ContohList() {
     //List mutable
     val shape: MutableList<String> = mutableListOf("Circlel", "square", "Triagle")
     println(shape)
+
+    //menambah data kedalam list mutable
+    shape.add("Circle")
+    println(shape)
+
+    //menghapus data dari list mutable
+    shape.remove("Triangle")
+    println(shape)
+
+    //mengubah data di dalam list mutable
+    shape[0] = "Oval"
+    println(shape)
+
+    //List Read-only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
 }
