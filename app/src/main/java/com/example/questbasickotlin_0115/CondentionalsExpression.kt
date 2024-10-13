4cdf19c7570a842f9bddcc722fbefe92c886caed
khaiqalsatrio <khaiqalsatrio29@gmail.com>
2024-10-13 09:20:58 +0700

    Tambahkan contoh pernyataan kondisional: if-else, when, dan try-catch

---
+++ app/src/main/java/com/example/questbasickotlin_0115/CondentionalsExpression.kt
@@ -0,0 +1,35 @@
+package com.example.questbasickotlin_0115
+
+fun CondintionalStatement(){
+    println("=== Condintional Expression ===")
+
+    //if-Else
+    val number = 10
+    if (number > 0){
+        println("Positive Number")
+    }else{
+        println("Negative Number")
+    }
+
+    //when
+    val day = 1
+    when (day){
+        1 -> println("Sunday")
+        2 -> println("Monday")
+        3 -> println("Tuesday")
+        4 -> println("Wednesday")
+        5 -> println("Thrusday")
+        6 -> println("Friday")
+        7 -> println("Saturday")
+        else -> println("Invalid day")
+    }
+
+    //try-Catch
+    val value = "10"
+    try{
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException){
+        println("Invalid Number")
+    }
+}
